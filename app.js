dcfdabfc7ea59172ecfc746c7eb03de952a12329
Aarya1219 <151129399+Aarya1219@users.noreply.github.com>
2024-08-30 10:37:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
 app.get("/",(req,res)=>{
-    res.send("hi,i am root");
+    res.send("listings/index");
 });
 
 app.all("*",(req,res,next)=>{
